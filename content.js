9d1fcff44102d2e776a0335bfb826c410db1839f
uberranger <ranger.e.davis@gmail.com>
2025-03-06 16:32:00 -0500

    updated dark mode

--- content.js
+++ content.js
@@ -7,84 +7,81 @@
     let docLinks = [];
     let expanded = false;
     
-    const darkModeStyle = document.createElement('style');
-    darkModeStyle.textContent = `:root, html, body, footer, .footer, footer div, .footer div, .panel, .form-control, .nav-tabs>li>a, .nav-tabs>li>a:hover, .nav-tabs>li.active>a,
-        .nav-tabs>li.active>a:hover, .navbar-inverse, nav.framework-header.navbar.navbar-inverse, div.nav-subrow, .alert-info, .btn-default, option {
-            // color-scheme: dark;
-            // background-color: unset;
-            background: unset;
-            background-color:black;
+    
+        
+    window.addEventListener("load", () => {
+        const darkModeStyle = document.createElement("style");
+        darkModeStyle.textContent = `@media (prefers-color-scheme: dark) {
+            :root, html, body, footer, .footer, footer div, .footer div, .panel, .form-control, .nav-tabs>li>a, .nav-tabs>li>a:hover, .nav-tabs>li.active>a,
+            .nav-tabs>li.active>a:hover, .navbar-inverse, nav.framework-header.navbar.navbar-inverse, div.nav-subrow, .alert-info, .btn-default, option {
+                background: unset;
+                background-color:black;
+                color: white;
+            }
+            div.nav-subrow {
+                background: unset !important;
+            }
+            .table-striped>tbody>tr:nth-of-type(odd) {
+                background-color: unset;
+            }
+            .modal-content, .modal-content .modal-header, .modal-content .modal-footer {
+                background-color: rgba(0, 0, 0, .8);
+            }
+            a {
+                color: deepskyblue;
+            }
+            .btn-primary, .btn-primary:hover {
+                background-color: deepskyblue;
+            }
+            tr:hover, .table-hover>tbody>tr:hover, .panel-default>.panel-heading {
+                background-color: #222;
+                color: white;
+            }
+            .text-primary {
+                color: lightblue;
+            }
+            .text-muted { 
+                color: lightgrey;
+            }
+            .nav-tabs>li.active>a, .nav-tabs>li.active>a:hover {
+                border-width: medium;
+            }
+            .list-group-item, .list-group-item-info {
+                background-color: black !important;
+                outline: 1px solid deepskyblue;
+                color: deepskyblue;
+            }            
+        }
+        @media (min-width: 1200px) {
+            .container {
+                width: 80%;
+            }
+        }`;
 
+        const toolContainerStyle = document.createElement("style");
+        toolContainerStyle.textContent = `
+        #toolContainer {
+            position: fixed;
+            top: 2vmax;
+            left: 2vmax;
+            z-index: 9999;
+            display: flex;
+            gap: 1vmax;
+        }
+        
+        #toolContainer > * {
+            padding: .5vmax;
+            fontSize: 1.25rem;
+            background: rgba(0, 0, 0, 0.75);
+            outline: 1px solid dodgerblue;
             color: white;
-        }
-        div.nav-subrow {
-            background: unset !important;
-        }
-        .table-striped>tbody>tr:nth-of-type(odd) {
-            background-color: unset;
-        }
-        .modal-content, .modal-content .modal-header, .modal-content .modal-footer {
-            background-color: rgba(0, 0, 0, .8);
-        }
-        a {
-            color: deepskyblue;
-        }
-        .btn-primary, .btn-primary:hover {
-            background-color: deepskyblue;
-        }
-        tr:hover, .table-hover>tbody>tr:hover, .panel-default>.panel-heading {
-            background-color: #222;
-            color: white;
-        }
-        .text-primary {
-            color: lightblue;
-        }
-        .text-muted { 
-            color: lightgrey;
-        }
-        .nav-tabs>li.active>a, .nav-tabs>li.active>a:hover {
-            border-width: medium;
+            cursor: pointer;
+            // box-shadow: 10px 5px 5px rgb(23, 68, 91);
         }`;
     
     document.head.appendChild(darkModeStyle);
+    document.head.appendChild(toolContainerStyle);
         
-    window.addEventListener("load", () => {
-        const buttonContainer = document.createElement("div");
-        buttonContainer.id = "mycaseToolContainer"
-        const downloadButton = document.createElement("button");
-        const refreshButton = document.createElement("button");
-        
-        const containerStyle = {
-            position: "fixed",
-            top: "2vmax",
-            left: "2vmax",
-            //left: "0",
-            zIndex: "9999",
-            display: 'flex',
-            gap: '1vmax',
-            //outline: '1px solid dodgerblue',
-            //padding: '.5vmax .5vmax .5vmax 2vmax',
-        };
-
-        const buttonStyle = {
-            padding: ".5vmax",
-            fontSize: "1.25rem",
-            background: "dodgerblue",
-            color: "white",
-            border: "none",
-            cursor: "pointer",
-            "box-shadow": "10px 5px 5px rgb(23, 68, 91)",
-        };
-
-        Object.assign(buttonContainer.style, containerStyle);
-        Object.assign(downloadButton.style, buttonStyle);
-        Object.assign(refreshButton.style, buttonStyle);
-
-        downloadButton.textContent = "No Documents Found";
-        refreshButton.textContent = "Refresh";
-
-        buttonContainer.appendChild(downloadButton);
-        buttonContainer.appendChild(refreshButton);
 
         const hasExpandTitle = (a) => a.getAttribute("title") && a.getAttribute("title").includes("Click to expand");
 
@@ -99,9 +96,9 @@
         }
 
         const checkDocLinks = () => {
-            highlightHearings();
+            highlightRows();
 
-            if (refreshAttempts >= maxRefreshAttempts) {
+            if (refreshAttempts >= maxRefreshAttempts) {                
                 downloadButton.textContent = "No Documents Found";
                 clearInterval(checkInterval);
                 return;
@@ -140,34 +137,45 @@
             });
         }
 
-        const highlightHearings = () => {
+        const highlightRows = () => {
             document.querySelectorAll("tr").forEach(tr => {
                 if (tr.textContent.toLowerCase().includes("session")) {
-                    tr.style.borderLeft = "10px solid firebrick";
+                    tr.style.borderLeft = "20px solid goldenrod";
                 } else if (tr.textContent.toLowerCase().includes("automated enotice")) {
-                    tr.style.borderLeft = "10px solid saddlebrown";
+                    tr.style.borderLeft = "5px solid saddlebrown";
                 } else if (tr.textContent.toLowerCase().includes("automated paper")) {
-                    tr.style.borderLeft = "10px solid midnightblue";
+                    tr.style.borderLeft = "5px solid midnightblue";
                 } else if (tr.textContent.toLowerCase().includes("appearance filed")) {
                     tr.style.borderLeft = "10px solid indigo";
+                } else if (tr.textContent.toLowerCase().includes("judicial officer")) {
+                    tr.style.borderLeft = "20px solid green";
                 }
             });
         }
 
-        const highlightNotices = () => {
-            document.querySelectorAll("tr").forEach(tr => {
-                
-            });
-        }
+        const buttonContainer = document.createElement("div");
+        const downloadButton = document.createElement("button");
+        const refreshButton = document.createElement("button");
 
         if (window.location.href.includes("vw/CaseSummary")) {
+            buttonContainer.id = "toolContainer"
+            
+            downloadButton.textContent = "No Documents Found";
+            
+            refreshButton.textContent = "Refresh";
+
+            buttonContainer.appendChild(downloadButton);
+            buttonContainer.appendChild(refreshButton);
+
+            downloadButton.addEventListener("click", downloadLinks);
+            refreshButton.addEventListener("click", refreshDocLinks);
+
             document.body.appendChild(buttonContainer);
-        } else if (document.getElementById("mycaseToolContainer")) {
+        } else if (document.getElementById("toolContainer")) {
             document.body.removeChild(buttonContainer);
         }
 
-        downloadButton.addEventListener("click", downloadLinks);
-        refreshButton.addEventListener("click", refreshDocLinks);
+        
 
         let refreshAttempts = 0;
         let checkInterval = setInterval(checkDocLinks, 250);
